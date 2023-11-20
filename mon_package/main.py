cea7a786478c9e15155b4edd2584f30572c001ab
EL Conquistador Baouly NELSON <96576850+BaoulyNelson@users.noreply.github.com>
2023-11-20 18:47:32 -0500

    Add files via upload

---
+++ mon_package/main.py
@@ -0,0 +1,6 @@
+from src.exemple_package.exemple import addition_des_nombres,multiplication_des_nombres
+
+# Appel de la fonction avec les valeurs 30 et 78
+addition_des_nombres(30, 78)
+
+multiplication_des_nombres(30, 78)
---
+++ mon_package/src/exemple_package/exemple.py
@@ -0,0 +1,21 @@
+#addistion
+def addition_des_nombres(nombre1, nombre2):
+    resultat = nombre1 + nombre2
+    print(f"La somme de {nombre1} et de {nombre2} est : {resultat}")
+
+    #multiplication
+def multiplication_des_nombres(nombre1, nombre2):
+    resultat = nombre1 * nombre2
+    print(f"Le produit de {nombre1} et de {nombre2} est : {resultat}")
+
+    #division
+def division_des_nombres(nombre1, nombre2):
+    resultat = nombre1 / nombre2
+    print(f"Le quotient de {nombre1} et de {nombre2} est : {resultat}")
+    
+   #soustraction     
+def soustraction_des_nombres(nombre1, nombre2):
+    resultat = nombre1 - nombre2
+    print(f"Le reste de {nombre1} et de {nombre2} est : {resultat}")
+
+    
